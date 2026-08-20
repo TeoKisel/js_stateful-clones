c9d458ab0b543d09d161d457c4100e7cc885723f
Timofei <kiselovtimofej@gmail.com>
2026-08-20 18:59:17 +0300

    Fix

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,37 +6,37 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
   const result = [];
-  const stateCopy = { ...state };
+  let currentState = { ...state };
 
   for (const action of actions) {
+    const stateCopy = { ...currentState };
+
     switch (action.type) {
       case 'addProperties':
         Object.assign(stateCopy, action.extraData);
-
-        result.push({ ...stateCopy });
         break;
 
       case 'removeProperties':
         for (const key of action.keysToRemove) {
           delete stateCopy[key];
         }
-
-        result.push({ ...stateCopy });
         break;
 
       case 'clear':
         for (const key in stateCopy) {
           delete stateCopy[key];
         }
-
-        result.push({ ...stateCopy });
         break;
 
       default:
-        break;
+        throw new Error(`Unknown action type: ${action.type}`);
     }
+
+    result.push(stateCopy);
+    currentState = stateCopy;
   }
 
   return result;
